3621212ee777ccb1d64fc9644d91c6ceb41ed442
Hamberfim <adhamlingithub@gmail.com>
2022-07-14 10:41:02 -0500

    annotated moderation comment categories

--- Enums/ModerationType.cs
+++ Enums/ModerationType.cs
@@ -1,13 +1,22 @@
-﻿namespace TheBlogProject.Enums
+﻿using System.ComponentModel;
+
+namespace TheBlogProject.Enums
 {
     public enum ModerationType
     {
+        [Description("Political")]
         Political,
+        [Description("Offensive Language")]
         Language,
+        [Description("Drug Reference")]
         Drugs,
+        [Description("Threatening Speach")]
         Threatening,
+        [Description("Sexual Content")]
         Sexual,
+        [Description("Hate Speech")]
         HateSpeech,
+        [Description("Targeted Shaming")]
         Shaming
     }
 }
